e83dd97870ee3d494b6360243df27904cc52af06
myxobek <myxobek@gmail.com>
2016-08-21 22:42:27 +0300

    Loads from ftp and saves to destination folder

--- src/RemoteImagesLoader.php
+++ src/RemoteImagesLoader.php
@@ -11,28 +11,34 @@ class RemoteImagesLoader
     const ERROR_CODE_CANNOT_LOGIN               = 2;
     const ERROR_CODE_CANNOT_SET_PASSIVE_MODE    = 3;
     
+    const DEFAULT_DESTINATION = './images/';
+    const DEFAULT_EXTENSIONS  = ['jpg', 'png', 'gif'];
+    
     ///////////////////////////////////////////////////////////////////////////
     
     private $host           = '';
     private $login          = '';
     private $password       = '';
     
-    private $extensions     = ['jpg', 'png', 'gif'];
+    private $extensions     = self::DEFAULT_EXTENSIONS;
     
-    private $destination    = './';
+    private $destination    = self::DEFAULT_DESTINATION;
     
-    private $connection     = false;
+    public $connection     = false;
+    
+    public $error_code     = self::ERROR_CODE_OK;
+    public $error_last_msg = '';
     
     ///////////////////////////////////////////////////////////////////////////
     
     /**
-     * RemoteImagesLoader constructor.
+     * Initializes package
      *
      * @author  myxobek
      *
      * @param   array   $data
      */
-    public function __construct( $data )
+    public function init( $data )
     {
         $this->host     = isset( $data['host'] )        ? $data['host']     : '';
         $this->login    = isset( $data['login'] )       ? $data['login']    : '';
@@ -46,24 +52,6 @@ class RemoteImagesLoader
         if ( isset( $data['destination'] ) && strlen( $data['destination'] ) > 0 )
         {
             $this->destination = $data['destination'];
-        }
-        
-        $error_code = $this->checkPackageRequirements();
-        
-        if ( $error_code !== self::ERROR_CODE_OK )
-        {
-            $common_error_message = 'Couldn\'t properly initialize package. Have a look at response below for more information:';
-            
-            $error_message = $this->getErrorText( $error_code );
-            
-            die(
-                implode('',
-                    [
-                        $common_error_message,
-                        "\n",
-                        $error_message
-                    ])
-            );
         }
     }
     
@@ -80,9 +68,9 @@ class RemoteImagesLoader
         $this->login        = '';
         $this->password     = '';
     
-        $this->extensions   = ['jpg', 'png', 'gif'];
+        $this->extensions   = self::DEFAULT_EXTENSIONS;
     
-        $this->destination  = './';
+        $this->destination  = self::DEFAULT_DESTINATION;
     
         $this->connection   = false;
     }
@@ -111,6 +99,9 @@ class RemoteImagesLoader
             case( self::ERROR_CODE_CANNOT_SET_PASSIVE_MODE ):
                 $result = 'Couldn\'t set passive mode';
                 break;
+            case( self::ERROR_CODE_OK ):
+                $result = 'Everything is fine';
+                break;
             default:
                 $result = 'Unknowm error';
                 break;
@@ -128,38 +119,285 @@ class RemoteImagesLoader
      *
      * @author      myxobek
      *
-     * @return      int
+     * @return      bool
      */
     public function checkPackageRequirements()
     {
-        $this->connection = ftp_connect( $this->host );
+        $this->error_code = $this->establishConnection();
         
-        // create connection
-        if ( $this->connection === false )
+        if ( $this->error_code !== self::ERROR_CODE_OK )
         {
-            return self::ERROR_CODE_UNKNOWN_HOST;
-        }
+            $common_error_message = 'Couldn\'t properly initialize package. Have a look at response below for more information:';
         
-        // try to login
-        if ( ! $login_result = @ftp_login( $this->connection, $this->login, $this->password ) )
-        {
-            return self::ERROR_CODE_CANNOT_LOGIN;
-        }
-    
-        // set passive mode
-        if ( ftp_pasv( $this->connection, true ) === false )
-        {
-            return self::ERROR_CODE_CANNOT_SET_PASSIVE_MODE;
-        }
+            $error_message = $this->getErrorText( $this->error_code );
         
-        return self::ERROR_CODE_OK;
+            $this->error_last_msg = (
+            implode('',
+                [
+                    $common_error_message,
+                    "\n",
+                    $error_message
+                ])
+            );
+            
+            return false;
+        }
+        else
+        {
+            $this->error_last_msg = $this->getErrorText( $this->error_code );
+            
+            return true;
+        }
     }
     
     ///////////////////////////////////////////////////////////////////////////
     
-    public function parse()
+    /**
+     * establishConnection
+     *
+     * @author      myxobek
+     *
+     * @return      int
+     */
+    public function establishConnection()
     {
+        $this->connection = @ftp_connect( $this->host );
         
+        // create connection
+        if ( $this->connection !== false )
+        {
+            // try to login
+            if ( $login_result = @ftp_login( $this->connection, $this->login, $this->password ) )
+            {
+                // set passive mode
+                if ( ftp_pasv( $this->connection, true ) !== false )
+                {
+                    $error_code = self::ERROR_CODE_OK;
+                }
+                else
+                {
+                    $error_code = self::ERROR_CODE_CANNOT_SET_PASSIVE_MODE;
+                }
+            }
+            else
+            {
+                $error_code = self::ERROR_CODE_CANNOT_LOGIN;
+            }
+        }
+        else
+        {
+            $error_code = self::ERROR_CODE_UNKNOWN_HOST;
+        }
+        
+        return $error_code;
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
+    /**
+     * getImages
+     *
+     * @author      myxobek
+     *
+     * @return      array|boolean
+     */
+    public function getImagesList()
+    {
+        $error_code = $this->establishConnection();
+        
+        if ( $error_code === self::ERROR_CODE_OK )
+        {
+            $list = ftp_rawlist( $this->connection, '.', true );
+    
+            $images = [];
+            $key    = '';
+    
+            // get files from root directory
+            $i = 0;
+            while( $list[$i] !== '' )
+            {
+                $file = $this->_getFIlename( $list[$i] );
+        
+                if ( $this->_isImage( $file ) )
+                {
+                    $images[$key][] = $file;
+                }
+        
+                ++$i;
+            }
+    
+            // get images from subdirectories
+            /**
+             * Get images from subdirectories
+             *
+             * Have a look at http://php.net/manual/ru/function.ftp-rawlist.php to see how ftp_rawlist returns list
+             */
+            for( $n = count( $list ); $i < $n; ++$i )
+            {
+                if ( $list[$i] == '' )
+                {
+                    $key = $this->_getDirectoryName( $list[$i+1] );
+                    ++$i;
+                }
+        
+                $j = $i+1;
+                while (
+                    ( $j < $n ) &&
+                    ( $list[$j] != '' )
+                )
+                {
+                    $file = $this->_getFIlename( $list[$j] );
+                    if ( $this->_isImage( $file ) )
+                    {
+                        $images[$key][] = $file;
+                    }
+            
+                    ++$j;
+                }
+        
+                // $i will become $i+1 on next iteration
+                $i = $j-1;
+            }
+            
+            return $images;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
+    /**
+     * downloadImages
+     *
+     * @author      myxobek
+     *
+     * @return      bool
+     */
+    public function downloadImages()
+    {
+        $images_list = $this->getImagesList();
+        
+        if ( $images_list !== false )
+        {
+            $this->createFolders( array_keys( $images_list ) );
+    
+            foreach ( $images_list as $directory => $images )
+            {
+                foreach ( $images as $image )
+                {
+                    $this->loadImage(
+                        implode(
+                            '',
+                            [
+                                $this->destination,
+                                $directory,
+                                '/',
+                                $image
+                            ]
+                        ),
+                        implode(
+                            '',
+                            [
+                                $directory,
+                                '/',
+                                $image
+                            ]
+                        )
+                    );
+                }
+            }
+            
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
+    /**
+     * loadImage
+     *
+     * @author      myxobek
+     *
+     * @param   string  $local_file
+     * @param   string  $remote_file
+     *
+     * @return  bool
+     */
+    public function loadImage( $local_file, $remote_file )
+    {
+        if ( ftp_get( $this->connection, $local_file, $remote_file, FTP_BINARY) )
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
+    /**
+     * createFolders
+     *
+     * @author      myxobek
+     *
+     * @param   array   $keys
+     */
+    public function createFolders( $keys )
+    {
+        foreach ( $keys as $directory )
+        {
+            exec( 'mkdir -p ' . $this->destination . $directory, $output, $return_var );
+        }
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
+    /**
+     * _getFIlename
+     *
+     * Strings are like: drwxr-x---   7 user group     4096 Jun 23 22:49 .
+     *
+     * @author      myxobek
+     *
+     * @param   string  $string
+     *
+     * @return mixed
+     */
+    private function _getFIlename( $string )
+    {
+        $string_as_array = explode( ' ', $string );
+        
+        return $string_as_array[ count( $string_as_array ) -1 ];
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
+    public function _isImage( $filename )
+    {
+        if ( preg_match( '#^.*?\.(' . implode('|', $this->extensions) . ')$#', $filename ) )
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
+    private function _getDirectoryName( $directory )
+    {
+        return substr( $directory, 0, -1);
     }
     
     ///////////////////////////////////////////////////////////////////////////
--- tests/RemoteImagesLoaderTest.php
+++ tests/RemoteImagesLoaderTest.php
@@ -4,14 +4,84 @@ use Myxobek\RemoteImagesLoader\RemoteImagesLoader;
 
 class RemoteImagesLoaderTest extends PHPUnit_Framework_TestCase
 {
+    private $data = [
+        'host'      => '',
+        'login'     => '',
+        'password'  => ''
+    ];
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
+    private function _getConfData()
+    {
+        $handle = fopen('.conf', 'r');
+        if ( $handle )
+        {
+            while( ( $line = fgets( $handle ) ) !== false)
+            {
+                list( $key, $value ) = explode( '=', $line );
+    
+                $this->data[$key] = substr( $value, 0, -1);
+            }
+        }
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->_getConfData();
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
+    public function testGetErrorText()
+    {
+        $remoteImagesLoader = new RemoteImagesLoader();
+        
+        $this->assertEquals($remoteImagesLoader->getErrorText( $remoteImagesLoader::ERROR_CODE_OK ), 'Everything is fine' );
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
     public function testCheckPackageRequirements()
     {
-        $remoteImagesLoader = new RemoteImagesLoader([
-            'host'          => '',
-            'login'         => '',
-            'password'      => '',
-            'extensions'    => ['jpg', 'png', 'gif'],
-            'destination'   => ''
-        ]);
+        $remoteImagesLoader = new RemoteImagesLoader();
+        
+        $this->assertFalse( $remoteImagesLoader->checkPackageRequirements() );
     }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
+    public function testGetImagesList()
+    {
+        $remoteImagesLoader = new RemoteImagesLoader();
+        
+        $remoteImagesLoader->init([
+            'host'      => $this->data['host'],
+            'login'     => $this->data['login'],
+            'password'  => $this->data['password'],
+        ]);
+        
+        $this->assertTrue( is_array( $remoteImagesLoader->getImagesList() ) );
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
+    public function testDownloadImages()
+    {
+        $remoteImagesLoader = new RemoteImagesLoader();
+    
+        $remoteImagesLoader->init([
+            'host'      => $this->data['host'],
+            'login'     => $this->data['login'],
+            'password'  => $this->data['password'],
+        ]);
+    
+        $this->assertTrue( $remoteImagesLoader->downloadImages() );
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
 }
